991b705e8fb21dbc98787eec250b2a924e9c612a
krist <s6krball@uni-bonn.de>
2021-08-19 20:19:52 +0200

    :sparkles: VL zu basics beim programmieren

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,24 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String studiName = "kristi";
+        String studiNachname = "balla";
+        int studiMitarbeiterNummer = 1111111;
+        boolean kristiCanHackYou = false;
+
+        String dozentName = "felix";
+        String dozentNachname = "boes";
+        int dozentMitarbeiterNummer = 42;
+        boolean felixCanHackYou = true;
+
+        String[][] namen = {{studiName, studiNachname}, {dozentName, dozentNachname}};
+
+        for (int i = 0; i < 2; i++) {
+
+            System.out.println("Durchlauf: " + (i + 1));
+            System.out.println("name = " + namen[i][0]
+                    + "\nnachname = " + namen[i][1] + "\n\n");
+        }
     }
 }
